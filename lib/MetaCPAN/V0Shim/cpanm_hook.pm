64a4dbf99ed192aaba7ca9194cb7f9c626798e1f
Graham Knop <haarg@haarg.org>
2017-06-16 00:27:22 +0200

    allow specifying log level in cpanm hook, and use colored output

--- lib/MetaCPAN/V0Shim/cpanm_hook.pm
+++ lib/MetaCPAN/V0Shim/cpanm_hook.pm
@@ -5,21 +5,37 @@ use warnings;
 use LWP::Protocol::PSGI;
 use MetaCPAN::V0Shim;
 use Plack::Builder;
+use Log::Contextual qw( set_logger );
+use Log::Log4perl ();
 
 my @hooks;
 
 sub import {
-  my ($class, @opts) = @_;
-  my %opts = map +($_ => 1), @opts;
+  my ($class, %opts) = @_;
+  return if @hooks;
+  if (!Log::Log4perl->initialized) {
+    Log::Log4perl->init(\<<'EOT');
+      log4perl.category = WARN, Screen
+
+      log4perl.appender.Screen = Log::Log4perl::Appender::ScreenColoredLevels
+      log4perl.appender.Screen.layout = Log::Log4perl::Layout::PatternLayout
+      log4perl.appender.Screen.layout.ConversionPattern = [%p] %m%n
+EOT
+  }
+
+  my $logger = Log::Log4perl->get_logger('MetaCPAN::V0Shim');
+  $logger->level(Log::Log4perl::Level::to_priority(uc $opts{log_level}))
+    if $opts{log_level};
+  set_logger($logger);
 
   my $shim = MetaCPAN::V0Shim->new;
-
   my $shim_app = builder {
     mount '/v0' => $shim->app;
     mount '/' => $shim->app;
   };
+
   push @hooks, LWP::Protocol::PSGI->register($shim_app, host => 'api.metacpan.org');
-  if ($opts{'disable-metadb'}) {
+  if ($opts{'disable_metadb'}) {
     my $blocked = sub { [ 500, [], [''] ] };
     push @hooks, LWP::Protocol::PSGI->register($blocked, host => 'cpanmetadb.plackperl.org');
   }
